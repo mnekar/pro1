fad6fbeb918d68bae28d7a51585b84f711b531c4
mnekar <manjunath11408@gmail.com>
2012-08-20 01:49:54 +0530

    essay continued..

--- essay.c
+++ essay.c
@@ -1,6 +1,8 @@
-                   ESSAY WRITING
+                              ESSAY WRITING
 Often , and for the best of the motives, our problems in essay writing begin the very minute we are given a question.Anxious to get with the work and not fall 
 behind, we skip the interpretation stage and launch straight into our research.
 As a result we raed resources,take notes without a clear idea of what is 
 relevant,beyond some very general idea of the subject of the essay.Then finally, after hours of toil,tired and frustrated and no clear about what we're left 
 with a file of irrelevant,unusable notes.
+                              Yet,just an hour or two interpreting the question
+would not only have saved us this wasted time, but would have given us a clear  idea of what the question is getting at and a better understanding of what te examiner is looking for i our work.And even more, it would have given us opportunities to get our own ideas and insights involved at the early stage.Without this our work can seem routine and predictable:at best just recycling of the ideas that dominate the subject. 
