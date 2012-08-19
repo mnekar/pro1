4266951d948fb13a9a45ec776faec97aa22ad394
mnekar <manjunath11408@gmail.com>
2012-08-20 01:28:14 +0530

    1st commit after initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ pro1
 ====
 
 CS251 project 1
+
+This was the file initially committed to the new repository I created in the 
+github.com and cloned with URL - https://github.com/mnekar/pro1.git .
+
+Things I have done,
+
---
+++ essay.c
@@ -0,0 +1,6 @@
+                   ESSAY WRITING
+Often , and for the best of the motives, our problems in essay writing begin the very minute we are given a question.Anxious to get with the work and not fall 
+behind, we skip the interpretation stage and launch straight into our research.
+As a result we raed resources,take notes without a clear idea of what is 
+relevant,beyond some very general idea of the subject of the essay.Then finally, after hours of toil,tired and frustrated and no clear about what we're left 
+with a file of irrelevant,unusable notes.
