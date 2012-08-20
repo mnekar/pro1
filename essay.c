78b44a3e3377b4d6fd14e17e222356b789f9fd9c
mnekar <manjunath11408@gmail.com>
2012-08-20 10:44:56 +0530

    essay para added..

--- essay.c
+++ essay.c
@@ -5,4 +5,9 @@ As a result we raed resources,take notes without a clear idea of what is
 relevant,beyond some very general idea of the subject of the essay.Then finally, after hours of toil,tired and frustrated and no clear about what we're left 
 with a file of irrelevant,unusable notes.
                               Yet,just an hour or two interpreting the question
-would not only have saved us this wasted time, but would have given us a clear  idea of what the question is getting at and a better understanding of what te examiner is looking for i our work.And even more, it would have given us opportunities to get our own ideas and insights involved at the early stage.Without this our work can seem routine and predictable:at best just recycling of the ideas that dominate the subject. 
+would not only have saved us this wasted time, but would have given us a clear  idea of what the question is getting at and a better understanding of what te examiner is looking for i our work.And even more, it would have given us opportunities to get our own ideas and insights involved at the early stage.Without this our work can seem routine and predictable:at best just recycling of the ideas that dominate the subject.
+ So,what should you be looking for when you interpret a question?All essay 
+questions tell you two thing:the structure you should adopt for you to deal 
+relevently with all the issues ;and the range of abilities the examiner is expecting you to use in answering a question.
+  Take first of these:'Structure'- to unwrap the meaning of a and implications 
+of a question.Most of us struggle to do this well,but the skills involved can be easily learnt.The simple three step technique for analysing the most difficult  concepts.
